9c471853b4a4c095456f2cebebd5cbe173134a3e
Sanjay <Sanjaydh006@gmail.com>
2025-06-22 18:43:31 +0530

    added product component in home

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { Catnavigation } from './catnavigation/catnavigation';
 import { SidebarNavigation } from '../sidebar-navigation/sidebar-navigation';
+import { Products } from "./products/products";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Catnavigation, SidebarNavigation],
+  imports: [Header, Catnavigation, SidebarNavigation, Products],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
